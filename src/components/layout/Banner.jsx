a33fcc213305ea562c043fb2fbc266f554b8263a
Bin <ptbinh203@gmail.com>
2025-06-02 11:30:42 +0800

    fix banner

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,16 +1,25 @@
-import './Banner.css'
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="banner">
-      <div className="banner-image">
-        <img src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/ff/8d/ff8d444f0ee95b8be4e88ed584ae861e.png" alt="Banner" />
-      </div>
-      <div className="banner-image">
-        <img src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/be/14/be1485daef7f9dddba6d0cae6acf9e4b.png" alt="Banner" />
+      <div className="banner-container">
+        <div className="banner-slide">
+          <img
+            src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/ff/8d/ff8d444f0ee95b8be4e88ed584ae861e.png"
+            alt="Banner"
+          />
+        </div>
+        <div className="banner-slide">
+          <img
+            src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Banner/be/14/be1485daef7f9dddba6d0cae6acf9e4b.png"
+            alt="Banner"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
+
